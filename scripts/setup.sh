b8ab346d0e31dca3dce8141e7fce7791567e125f
Ske-Man1149 <128849789+Ske-Man1149@users.noreply.github.com>
2025-01-17 22:03:42 +0200

    Delete scripts/setup.sh

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/Ske-Man1149/hyprland-script-setup.git
-cd hyprland-script-setup
-cp -r rofi/ ~/.config && cp -r scripts/ ~/.config/hypr
-chmod +x ~/.config/hypr/scripts/RofiEmoji.sh
-chmod +x ~/.config/hypr/scripts/WallpaperSelect.sh
-chmod +x ~/.config/hypr/scripts/rofi_app_menu.sh
-mkdir ~/.config/hypr/wallpaper
