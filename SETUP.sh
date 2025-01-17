927b439a3492dbc0769901b7f402fdd5fabcf49a
Ske-Man1149 <128849789+Ske-Man1149@users.noreply.github.com>
2025-01-18 00:43:29 +0200

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -6,3 +6,4 @@ chmod +x ~/.config/hypr/scripts/RofiEmoji.sh
 chmod +x ~/.config/hypr/scripts/WallpaperSelect.sh
 chmod +x ~/.config/hypr/scripts/rofi_app_menu.sh
 mkdir ~/.config/hypr/wallpaper
+cp test_for_wallpaper_script.png ~/.config/hypr/wallpaper
