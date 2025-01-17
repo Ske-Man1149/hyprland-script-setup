6968cc26ab2a6de4eabc5266efd1537361ac9a25
Ske-Man1149 <128849789+Ske-Man1149@users.noreply.github.com>
2025-01-17 22:01:05 +0200

    Create SETUP.sh

---
+++ SETUP.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Ske-Man1149/hyprland-script-setup.git
+cd hyprland-script-setup
+cp -r rofi/ ~/.config && cp -r scripts/ ~/.config/hypr
+chmod +x ~/.config/hypr/scripts/RofiEmoji.sh
+chmod +x ~/.config/hypr/scripts/WallpaperSelect.sh
+chmod +x ~/.config/hypr/scripts/rofi_app_menu.sh
+mkdir ~/.config/hypr/wallpaper
